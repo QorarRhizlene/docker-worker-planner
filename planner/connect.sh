9f6797cba2caef090deb7b90be1e7b653a9efb83
QorarRhizlene <rhizlene.qorar0605@gmail.com>
2023-11-26 18:18:55 +0100

    exo 1

--- planner/connect.sh
+++ planner/connect.sh
@@ -1,4 +1,4 @@
 for var in "$@"
 do
-    curl -X POST localhost:3000/register  -H "Content-Type: application/json"  -d "{\"url\": \"http://localhost:$var\"}"
+    curl -X POST localhost:3000/register  -H "Content-Type: application/json"  -d "{\"url\": \"http://172.26.0.2:$var\"}"
 done
